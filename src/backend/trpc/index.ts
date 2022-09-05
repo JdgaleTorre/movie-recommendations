8af260c37b5d28dfc5171a1c1652650053b1b8c2
Jose David Gale <josedavidgale@gmail.com>
2022-09-05 20:03:38 +0100

    feat: adding petition to TMDB

--- src/backend/trpc/index.ts
+++ src/backend/trpc/index.ts
@@ -1,15 +1,30 @@
 import * as trpc from "@trpc/server";
 import { z } from "zod";
 
-export const appRouter = trpc.router().query("hello", {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
+type TMDB_movie = {
+  id: number;
+  title: string;
+  vote_count: number;
+  poster_path: string | null;
+  vote_average: number;
+};
+
+export const appRouter = trpc.router().query("getPopular", {
+  async resolve() {
+    let response = await fetch(
+      `${process.env.TMDB_URL}movie/popular?api_key=${process.env.TMDB_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((response) => response);
+
+    // console.log(response);
     return {
-      greeting: `hello ${input?.text ?? "world"}`,
+      movies: response.results.map((el: TMDB_movie) => {
+        return {
+          ...el,
+          poster_path: `https://www.themoviedb.org/t/p/w220_and_h330_face${el.poster_path}`,
+        };
+      }) as TMDB_movie[],
     };
   },
 });
